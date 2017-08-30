1c032882f182a528690c2685e048dd88fdcadd0a
Wichuda Kittikungsadarn <wichuda.k@motif.corp>
2017-08-30 23:58:16 +0700

    no message

--- Hello.java
+++ Hello.java
@@ -2,10 +2,8 @@
 class Hello {
     
     public Hello() {
-<<<<<<< HEAD
         System.out.println("Pornthep Singto");
         //this.Krerk();
-=======
         System.out.println("Hello! We are:");
 
         this.Onsiri();
@@ -14,23 +12,18 @@ class Hello {
 		this.Piyanat() ;
 		this.Sujinno();
         this.Panjapoll();
-<<<<<<< HEAD
-	this.Chananya();
-this.Chadaporn();
+		this.Chananya();
+		this.Chadaporn();
         this.Chananya();
         this.Natpapat();
-this.Adichart();
-=======
-<<<<<<< HEAD
->>>>>>> e6bc3e27bab545c1a5951c5314a862d0f92e174e
+		this.Adichart();
+		this.Wichuda();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("  Demo  - Krerk Piromsopa, Ph.d.");
-=======
 		this.Adichart();
->>>>>>> b7ca49afa1bfdf559bc8a8946fb3ca008d44b3ec
 		this.Sittikorn();
 }
 
@@ -39,10 +32,9 @@ this.Adichart();
     private void Onsiri() {
         System.out.println("Trainer - Onsiri Homsup2");
         System.out.println("  Demo  - Onsiri Homsup2");
->>>>>>> 52a15a280a3e73f24e5722a7369c4d53dc8afb79
     }
 	
-private void Piyanat(){
+private void Chadaporn(){
         System.out.println("  Trainee  - Chadaporn Sukcham, ASA");
 	}
 	private void Piyanat(){
@@ -56,11 +48,9 @@ private void Piyanat(){
     private void Panjapoll() {
         System.out.println("Trainee - Panjapol Lomarat, SSE");
     }
-<<<<<<< HEAD
 	private void Piyaporn() {
         System.out.println("Trainee - Piyaporn Eiamsakul, Miss");
     }
-=======
 	
 	private void Adichart() {
 		System.out.println("Trainee - Adichart Chanhomhaun, SE");
@@ -69,17 +59,10 @@ private void Piyanat(){
 	private void Sittikorn() {
         System.out.println("Trainer - Sittikorn Chuensukprasong, SE");
     }
-
-<<<<<<< HEAD
->>>>>>> 52a15a280a3e73f24e5722a7369c4d53dc8afb79
-=======
->>>>>>> ae62afdf76a9485c9f15ef54cd26af878fa74520
->>>>>>> 32998ac64215d9562081e46e3b409031918bd975
     
     public static void main(String [] args) {
         new Hello();
     }
-<<<<<<< HEAD
 	
 	private void Chananya() {
         System.out.println("Trainer - Chananya Phugsuwan, SE");
@@ -87,7 +70,10 @@ private void Piyanat(){
 	private void Natpapat() {
         System.out.println("Trainer - Natpapat Kwaopiwong, ASA");
     }
+	private void Wichuda() {
+        System.out.println("Trainer - Wichuda Kitikungsadarn, ASA");
+    }
 }
-=======
-}
->>>>>>> b7ca49afa1bfdf559bc8a8946fb3ca008d44b3ec
+
+
+
